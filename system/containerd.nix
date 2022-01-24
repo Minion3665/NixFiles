ec8eb5764f238d8fc2fce49dfd7c29a7d8187e3c
Skyler Turner <skyler@clicksminuteper.net>
2022-01-24 11:27:39 +0000

    Committed after system build

--- system/containerd.nix
+++ system/containerd.nix
@@ -1,10 +1,15 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, environment, ... }: {
   virtualisation.containerd.enable = true;
 
   virtualisation.containerd.settings = {
     version = 2;
-    grpc = {
-      uid = 1000;
-    };
+#    grpc = {
+#      uid = 1000;
+#    };
   };
+
+  environment.systemPackages = with pkgs; [
+    cni
+    cni-plugins
+  ];
 }
