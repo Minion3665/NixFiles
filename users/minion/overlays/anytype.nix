508131259dd5d3cf0800f5433c22030d7c2e561e
Skyler Turner <skyler@clicksminuteper.net>
2022-01-28 14:26:41 +0000

    Committed after home build

--- users/minion/overlays/anytype.nix
+++ users/minion/overlays/anytype.nix
@@ -11,19 +11,29 @@ final: prev: let
     name = "AnyType-${build}.AppImage";
   };
 
-  contents = final.appimageTools.extractType2 { inherit name src; };
+  appimageContents = final.appimageTools.extractType2 { inherit name src; };
 in {
-  anytype-latest = prev.anytype.overrideAttrs (old: {
-    version = build;
+  anytype-latest = prev.appimageTools.wrapType2 {
     inherit name src;
+
+    extraPkgs = pkgs: (prev.appimageTools.defaultFhsEnvArgs.multiPkgs pkgs)
+      ++ [ pkgs.libsecret ];
 
     extraInstallCommands = ''
       mv $out/bin/${name} $out/bin/${pname}
-      install -m 444 -D ${contents}/anytype2.desktop -t $out/share/applications
+      install -m 444 -D ${appimageContents}/anytype2.desktop -t $out/share/applications
       substituteInPlace $out/share/applications/anytype2.desktop \
-        --replace 'Exec=AppRun' 'Exec=${name}'
-      install -m 444 -D ${contents}/usr/share/icons/hicolor/0x0/apps/anytype2.png \
+      --replace 'Exec=AppRun' 'Exec=${pname}'
+      install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/0x0/apps/anytype2.png \
         $out/share/icons/hicolor/512x512/apps/anytype2.png
     '';
-  });
+
+    meta = with prev.lib; {
+      description = "P2P note-taking tool";
+      homepage = "https://anytype.io/";
+      license = licenses.unfree;
+      maintainers = with maintainers; [ bbigras ];
+      platforms = [ "x86_64-linux" ];
+    };
+  };
 }
