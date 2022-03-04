f41070242322d28f982e601b1f5e4b47914798dc
Skyler Turner <skyler@clicksminuteper.net>
2022-03-04 22:22:28 +0000

    Added the rebuild-home script and fix flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
         home-manager.url = "github:nix-community/home-manager/release-21.11";
     };
 
-    outputs = { self, nixpkgs, home-manager }: {
+    outputs = { self, nixpkgs, home-manager }:
         let
             system = "x86_64-linux";  # TOOD: Add options for MacOS
 
