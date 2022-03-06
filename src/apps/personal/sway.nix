f5bdb680ca4361c5a980df013de606305f81220b
Skyler Turner <skyler@clicksminuteper.net>
2022-03-06 20:04:08 +0000

    Turn after into a string

--- src/apps/personal/sway.nix
+++ src/apps/personal/sway.nix
@@ -15,6 +15,6 @@
         documentation = [ "man:systemd.special(7)" ];
         bindsTo = [ "graphical-session.target" ];
         wants = [ "graphical-session-pre.target" ];
-        after = [ "graphical-session-pre.target" ];
+        after = "graphical-session-pre.target";
     };
 }
