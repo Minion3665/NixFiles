80724b2d803391fedc05760d756283233c36a13d
Skyler Turner <skyler@clicksminuteper.net>
2022-03-07 21:44:04 +0000

    Set inner margins to 0

--- src/apps/personal/waybar.nix
+++ src/apps/personal/waybar.nix
@@ -13,8 +13,8 @@
                 padding-left: 10px;
                 padding-right: 10px;
                 border-radius: 5px;
-                margin-top: 10px;
-                margin-bottom: 10px;
+                margin: 10px 0;
+                transition: all 0.5s;
             }
 
             window#waybar * * * *:first-child {
