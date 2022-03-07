f367249f148cfe5fc877c4508ebb2584b8c72ee0
Skyler Turner <skyler@clicksminuteper.net>
2022-03-07 22:36:10 +0000

    Update padding to 5px again

--- src/apps/personal/waybar.nix
+++ src/apps/personal/waybar.nix
@@ -25,7 +25,7 @@
                 padding-left: 10px;
                 padding-right: 10px;
                 border-radius: 5px;
-                margin: 10px 0;
+                margin: 10px 5px;
             }
 
             window#waybar * * *:first-child * {
