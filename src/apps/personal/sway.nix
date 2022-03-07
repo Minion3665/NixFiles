cf4510895b44c8a1fbe785faed23c6c03734c5e3
Skyler Turner <skyler@clicksminuteper.net>
2022-03-07 13:41:29 +0000

    Shrink outer gaps

--- src/apps/personal/sway.nix
+++ src/apps/personal/sway.nix
@@ -14,7 +14,7 @@
             fonts = {};
             gaps = {
                 inner = 10;
-                outer = 10;
+                outer = 5;
                 smartGaps = true;
             };
             input = {
