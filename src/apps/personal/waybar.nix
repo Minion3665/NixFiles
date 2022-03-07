fd265098ade6c8c93a502cb1ea05ef09bbc0803e
Skyler Turner <skyler@clicksminuteper.net>
2022-03-07 21:31:38 +0000

    That was *not* 10px of padding

--- src/apps/personal/waybar.nix
+++ src/apps/personal/waybar.nix
@@ -10,7 +10,8 @@
             window#waybar * * * {
                 background: @theme_base_color;
                 color: @theme_text_color;
-                padding: 10px;
+                padding-left: 10px;
+                padding-right: 10px;
                 border-radius: 5px;
             }
         '';
