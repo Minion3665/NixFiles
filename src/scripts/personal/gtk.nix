304abb2a4f24007bc0255f4e5ff5d903f0fdde4f
Skyler Turner <skyler@clicksminuteper.net>
2022-03-08 13:30:56 +0000

    Add dark theme

--- src/scripts/personal/gtk.nix
+++ src/scripts/personal/gtk.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }: {
     gtk = {
         enable = true;
+        
+        gtk3.extraConfig = { gtk-application-prefer-dark-theme = true; };
+        # gtk4.extraConfig = { gtk-application-prefer-dark-theme = true; }; # Does not exist in my version of home-manager
         theme = {
-            package = pkgs.juno-theme;
-            name = "Juno";
+            name = "Adwaita-Dark";
+            package = pkgs.gnome.gnome_themes_standard;
         };
     };
 }
