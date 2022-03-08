7145c2dd4f9367b095c4b984b621fc8c29062b16
Skyler Turner <skyler@clicksminuteper.net>
2022-03-08 19:19:10 +0000

    Committed after system build

--- system/configuration.nix
+++ system/configuration.nix
@@ -226,6 +226,8 @@
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
 
+  packages.dconf.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.minion = {
     isNormalUser = true;
