32a415f8cec47c1e4bc3117ec723dbb1116004f4
Skyler Turner <skyler@clicksminuteper.net>
2022-03-13 11:20:27 +0000

    Add proton-caller

--- src/apps/personal/steam.nix
+++ src/apps/personal/steam.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
     home.packages = with pkgs; [
         steam-run
+        proton-caller
     ];
 
     # Due to opening the firewall and creating udev rules,
@@ -8,4 +9,7 @@
 
     # Steam-run is a FHS-compliant wrapper (around any executable), which
     # we can run here, but the main steam package is not here
+
+    # Proton-caller is a windows-exe caller which runs windows programs using
+    # Proton
 }
