64f7290e6fdcb8903ece2b578519bbb434ad9d76
Skyler Turner <skyler@clicksminuteper.net>
2022-03-17 10:48:20 +0000

    Add the sha to the download

--- src/apps/personal/overlays/hubfs.nix
+++ src/apps/personal/overlays/hubfs.nix
@@ -3,6 +3,7 @@ final: prev: let
     owner = "winfsp";
     repo = "hubfs";
     rev = "1ba013d3fe54de01c52bd74d98037fe4c0029d6e";
+    hash = "sha256-R1nCdua0gacXrglQ4AZfxnO3ngVECCKKiUOgp3dWRGg=";
   } + "/src";
 in {
   hubfs = final.buildGoModule {
