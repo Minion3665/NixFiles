67d911a92b3bbf26e3ea384c840f53610b478b29
Skyler Turner <skyler@clicksminuteper.net>
2022-03-17 08:54:58 +0000

    Add hubfs overlay

---
+++ src/apps/personal/overlays/hubfs.nix
@@ -0,0 +1,11 @@
+final: prev: let
+  src = final.fetchFromGitHub {
+    owner = "winfsp";
+    repo = "hubfs";
+    rev = "1ba013d3fe54de01c52bd74d98037fe4c0029d6e";
+  } + "/src";
+in {
+  hubfs = buildGoModule {
+    inherit src;
+  }
+}
