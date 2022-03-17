dca1df3a591a5a81106980fb7d5bc5d81c7ee6b3
Skyler Turner <skyler@clicksminuteper.net>
2022-03-17 23:57:11 +0000

    Change the hubfs derivation for a make building one as prev didn't work with fuse

--- src/apps/personal/overlays/hubfs.nix
+++ src/apps/personal/overlays/hubfs.nix
@@ -1,19 +1,32 @@
 final: prev: let
+  version = "1ba013d3fe54de01c52bd74d98037fe4c0029d6e";
+
   src = final.fetchFromGitHub {
     owner = "winfsp";
     repo = "hubfs";
-    rev = "1ba013d3fe54de01c52bd74d98037fe4c0029d6e";
+    rev = version;
     hash = "sha256-R1nCdua0gacXrglQ4AZfxnO3ngVECCKKiUOgp3dWRGg=";
-  } + "/src";
+  };
 in {
-  hubfs = final.buildGoModule {
+  hubfs = final.stdenv.mkDerivation {
     name = "hubfs";
+    inherit src, version;
+    rev = version;
+
     buildInputs = with final; [
         fuse
+        fuse-common
+        go
+        cmake
     ];
-    checkPhase = ":"; # Bit of a hack here; we need to disable tests as we can't get FUSE inside the build derivation to test the package
-    inherit src;
-    vendorSha256 = "sha256-Fpa+wanlMIqxkEZ3JQdCT4ixuNBj7AquG2+wLuO5TQU=";
-    runVend = true;
+
+    buildPhase = ''
+      make
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      mv hubfs $out/bin
+    '';
   };
 }
