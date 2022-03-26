6cd02e30aac22f14566440e3ed5c93a114d86e67
Skyler Turner <skyler@clicksminuteper.net>
2022-03-26 11:25:26 +0000

    Committed after system build

--- system/packaging-configuration.nix
+++ system/packaging-configuration.nix
@@ -1,8 +1,8 @@
 { config, pkgs, options, ... }: {
   # Apply custom packaging overlays for compatability
-  nix.nixPath =
-    # Prepend default nixPath values.
-    options.nix.nixPath.default ++
-    # Append our nixpkgs-overlays.
-    [ "nixpkgs-overlays=/etc/nixos/overlays/" ];
+#  nix.nixPath =
+#    # Prepend default nixPath values.
+#    options.nix.nixPath.default ++
+#    # Append our nixpkgs-overlays.
+#    [ "nixpkgs-overlays=/etc/nixos/overlays/" ];
 }
