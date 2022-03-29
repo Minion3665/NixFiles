d4a6bb4c4e4fb1eceef26ec774da5b8ceab9acd1
Skyler Turner <skyler@clicksminuteper.net>
2022-03-29 19:21:08 +0100

    Add brackets around that override

--- src/apps/personal/figma.nix
+++ src/apps/personal/figma.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
     home.packages = with pkgs; [
-        figma-linux.override { fonts = [ open-fonts roboto roboto-mono nerdfonts ]; }
+        (figma-linux.override { fonts = [ open-fonts roboto roboto-mono nerdfonts ]; })
     ];
 }
