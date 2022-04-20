0a93c758435ac525329d1be143fb51ee0e9a6496
Skyler Turner <skyler@clicksminuteper.net>
2022-04-20 22:12:27 +0100

    Change src to a string

--- src/apps/personal/packages/oneshot-game.nix
+++ src/apps/personal/packages/oneshot-game.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: pkgs.appimageTools.wrapType1 {
   name = "OneShot";
-  src = ./oneshot-game/OneShot.AppImage;
+  src = "./oneshot-game/OneShot.AppImage";
   extraPkgs = pkgs: with pkgs; [ ];
 }
