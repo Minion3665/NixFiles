d5377ef411210008a37aaeb0226791a324b9533c
Skyler Turner <skyler@clicksminuteper.net>
2022-04-20 23:14:50 +0100

    Add the right sha

--- src/apps/personal/packages/pridefetch.nix
+++ src/apps/personal/packages/pridefetch.nix
@@ -4,5 +4,6 @@
         owner = "SpyHoodle";
         repo = "pridefetch";
         rev = "bb57d32e7a5368a68e8abca0b3059bf2e491c247";
+        sha256 = "sha256-sYlt2LZHYSdHCmUPOi4vP/Tg2YNkCWSDoQxFKf5yQSM=";
     };
 }
