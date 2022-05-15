2d726b5565b90300449c3cffbdcdef8024de5d19
Skyler Turner <skyler@clicksminuteper.net>
2022-05-15 18:37:03 +0100

    Update comma (part 2 of 2)

--- src/apps/personal/comma.nix
+++ src/apps/personal/comma.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 let
-    comma = import ( pkgs.fetchFromGitHub {
+    comma = (import ( pkgs.fetchFromGitHub {
         owner = "nix-community";
         repo = "comma";
         rev = "034a9ca440370fc1eccbed43ff345fb6ea1f0d27";
-        sha256 = "sha256-WBIQmwlkb/GMoOq+Dnyrk8YmgiM/wJnc5HYZP8Uw72E=";
-    }) {};
+        sha256 = "sha256-CZQLigKovJbMoTbPuWT3SBSMymjD58m72O/oy+HilQg=";
+    })).default;
 in {
     home.packages = [ comma ];
 }
