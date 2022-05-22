16fcedd0a56bef358632de1740fff7920230b9e7
Skyler Grey <skyler@clicksminuteper.net>
2022-05-22 09:13:02 +0100

    Make home rebuild script remove old home-manager-path installs

--- scripts/rebuild-home.sh
+++ scripts/rebuild-home.sh
@@ -9,4 +9,14 @@ if [[ $(git diff --stat HEAD) != '' ]]; then
     /usr/bin/env git commit -am "$changes"
 fi
 
-home-manager switch --flake .#$USER $1
+nix build .#homeConfigurations.${USER}.activationPackage $1
+
+nix profile list \
+  | { grep 'home-manager-path$' || test $? = 1; } \
+  | awk -F ' ' '{ print $4 }' \
+  | cut -d ' ' -f 4 \
+  | xargs -t $DRY_RUN_CMD nix profile remove $VERBOSE_ARG
+# ^ Remove old profiles; from https://github.com/nix-community/home-manager/blob/8d38ca886880265d523a66fe3da4d42e92ab0748/modules/home-environment.nix#L582
+
+result/activate
+unlink result
