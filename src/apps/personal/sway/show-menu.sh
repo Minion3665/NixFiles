116031921702af5b2f635bced7f2232333746678
Skyler Grey <skyler3665@gmail.com>
2022-05-26 22:19:41 +0100

    Make the dmenu for showing scratchpad case insensitive

--- src/apps/personal/sway/show-menu.sh
+++ src/apps/personal/sway/show-menu.sh
@@ -1,3 +1,3 @@
-export id="$(swaymsg -t get_tree | jq -r '.nodes[].nodes[] | select(.name == "__i3_scratch").floating_nodes[] | ((.id | tostring) + "\t" + (.app_id // .window_properties.class) + "\t" + .name)' | rofi -dmenu | awk '{print $1;}')"
+export id="$(swaymsg -t get_tree | jq -r '.nodes[].nodes[] | select(.name == "__i3_scratch").floating_nodes[] | ((.id | tostring) + "\t" + (.app_id // .window_properties.class) + "\t" + .name)' | rofi -dmenu -i | awk '{print $1;}')"
 echo Showing $id
 swaymsg "[con_id=$id]" focus
