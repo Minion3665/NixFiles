da2879736a84da3e80cac0102c966d884f9f0225
Skyler Grey <skyler3665@gmail.com>
2022-05-30 23:56:42 +0100

    emacsPackagesNgGen -> emacsPackagesFor

--- src/system.nix
+++ src/system.nix
@@ -254,7 +254,7 @@ in {
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim  # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    ((emacsPackagesNgGen emacs).emacsWithPackages (epkgs: [
+    ((emacsPackagesFor emacs).emacsWithPackages (epkgs: [
       epkgs.vterm
       epkgs.emacsql-sqlite
     ]))  # Emacs + vterm-module (needed for vterm)
