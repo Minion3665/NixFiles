f7a2ac4ab1ab2d3ab7e7308a6b922bd1bd390fb2
Skyler Grey <skyler3665@gmail.com>
2022-06-04 16:53:25 +0100

    Update the ls command to better handle color aliases

--- src/apps/personal/ls.nix
+++ src/apps/personal/ls.nix
@@ -5,7 +5,11 @@
 
   programs.zsh.initExtra = ''
     function ls {
-      ${pkgs.exa}/bin/exa --icons -lghF --git --group-directories-first "$@" --color=always | less --quit-if-one-screen
+      ${pkgs.exa}/bin/exa --icons -lghF --git --group-directories-first --color always "$@" | less --quit-if-one-screen
     }
   '';
+
+  home.shellAliases = {
+    ls = "ls";  # Unset the default ls alias
+  };
 }
