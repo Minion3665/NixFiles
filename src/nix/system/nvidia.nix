e971985dacffdc61431b5c73ae0601caed90d8a2
Skyler Grey <skyler3665@gmail.com>
2022-06-05 20:14:15 +0100

    Install egl-wayland with the hope of fixing my external display

--- src/nix/system/nvidia.nix
+++ src/nix/system/nvidia.nix
@@ -1,8 +1,12 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.modesetting.enable = true;
 
   hardware.opengl.enable = true;
+
+  environment.systemPackages = [
+    pkgs.egl-wayland
+  ];
 
   ## Not yet finished; see https://nixos.wiki/wiki/Nvidia
   # TODO: Complete this
