d63fcd588702a5907814d195e9d8dadd8b57557e
Skyler Grey <skyler3665@gmail.com>
2022-06-06 11:36:58 +0100

    Make a todo command

---
+++ src/apps/personal/grep.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+  home.packages = [
+    pkgs.ripgrep
+    pkgs.gnugrep
+  ];
+
+  home.shellAliases = {
+    "git-todo" = "${pkgs.git}/bin/git log | ${pkgs.gnugrep}/bin/grep 'TODO\|DONE'";
+    todo = "${pkgs.ripgrep}/bin/rg TODO:";
+  };
+}
