86528ae45089405295d9686efb94262d5a738b71
Skyler Grey <skyler3665@gmail.com>
2022-06-10 19:50:03 +0100

    Add an egl-wayland config to nvidia

--- src/nix/system/nvidia.nix
+++ src/nix/system/nvidia.nix
@@ -8,6 +8,13 @@
     pkgs.egl-wayland
   ];
 
+  environment.etc."/egl/egl_external_platform.d/nvidia_wayland.json".text = ''
+    "file_format_version" : "1.0.0",
+    "ICD": {
+      "library_path" : "/run/opengl-driver/lib/libnvidia-egl-wayland.so"
+    }
+  '';
+
   ## Not yet finished; see https://nixos.wiki/wiki/Nvidia
   # TODO: Complete this
 }
