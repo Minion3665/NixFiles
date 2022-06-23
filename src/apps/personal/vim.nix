48028522a2eb5d8d7b274b49470780e27b6b55f3
Skyler Grey <skyler3665@gmail.com>
2022-06-23 23:37:22 +0100

    change the nvim status line

--- src/apps/personal/vim.nix
+++ src/apps/personal/vim.nix
@@ -22,6 +22,19 @@
         let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
         set termguicolors
       endif
+
+      set statusline=%t       "tail of the filename
+      set statusline+=[%{strlen(&fenc)?&fenc:'none'}, "file encoding
+      set statusline+=%{&ff}] "file format
+      set statusline+=%h      "help file flag
+      set statusline+=%m      "modified flag
+      set statusline+=%r      "read only flag
+      set statusline+=%y      "filetype
+      set statusline+=%=      "left/right separator
+      set statusline+=%c,     "cursor column
+      set statusline+=%l/%L   "cursor line/total lines
+      set statusline+=\ %P    "percent through file
+
     '';
     plugins = [
       pkgs.vimPlugins.vim-nix
