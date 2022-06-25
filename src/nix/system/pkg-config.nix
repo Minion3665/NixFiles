a7c2c51e2de6e1e533746ade23ddfc8fdb85e018
Skyler Grey <skyler3665@gmail.com>
2022-06-25 20:57:29 +0100

    Add an environment variable so pkg-config can find openssl

--- src/nix/system/pkg-config.nix
+++ src/nix/system/pkg-config.nix
@@ -2,4 +2,8 @@
   environment.systemPackages = [
     pkgs.pkg-config
   ];
+
+  environment.variables.PKG_CONFIG_PATH = builtins.concatStringsSep ":" [
+    "${pkgs.openssl.dev}/lib/pkgconfig"
+  ];
 }
