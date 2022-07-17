ef54c2db71ecfaf684310a9325218ea0e0b1bf0d
Skyler Grey <skyler3665@gmail.com>
2022-07-17 20:22:09 +0100

    Disable nvidia, return to noveau

--- src/nix/system/nvidia.nix
+++ src/nix/system/nvidia.nix
@@ -1,19 +1,19 @@
 { pkgs, ... }: {
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.modesetting.enable = true;
+  # services.xserver.videoDrivers = [ "noveau" ];
+  # hardware.nvidia.modesetting.enable = true;
 
-  hardware.opengl.enable = true;
+  # hardware.opengl.enable = true;
 
-  environment.systemPackages = [
-    pkgs.egl-wayland
-  ];
+  # environment.systemPackages = [
+  #   pkgs.egl-wayland
+  # ];
 
-  environment.etc."/egl/egl_external_platform.d/nvidia_wayland.json".text = ''
-    "file_format_version" : "1.0.0",
-    "ICD": {
-      "library_path" : "/run/opengl-driver/lib/libnvidia-egl-wayland.so"
-    }
-  '';
+  # environment.etc."/egl/egl_external_platform.d/nvidia_wayland.json".text = ''
+  #   "file_format_version" : "1.0.0",
+  #   "ICD": {
+  #     "library_path" : "/run/opengl-driver/lib/libnvidia-egl-wayland.so"
+  #   }
+  # '';
 
   ## Not yet finished; see https://nixos.wiki/wiki/Nvidia
   # TODO: Complete this
