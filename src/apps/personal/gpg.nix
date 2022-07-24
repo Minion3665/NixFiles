3bbe02b94aed9e86ed59342c8e049dcd844768cc
Skyler Grey <skyler3665@gmail.com>
2022-07-24 10:40:38 +0100

    Update gpg to use an env variable for pinentry

--- src/apps/personal/gpg.nix
+++ src/apps/personal/gpg.nix
@@ -1,11 +1,27 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+    pinentry-multiplexed = pkgs.writeScriptBin "pinentry-multiplexed" ''
+        case $PINENTRY_USER_DATA in
+        tty)
+            exec ${pkgs.pinentry.tty}/bin/pinentry "$@"
+            ;;
+        none)
+            exit 1
+            ;;
+        *)
+            exec ${pkgs.pinentry.qt}/bin/pinentry "$@"
+        esac
+    '';
+in {
     programs.gpg.enable = true;
     services.gpg-agent = {
         enable = true;
-        pinentryFlavor = "qt";
+        extraConfig = ''
+          pinentry-program ${pinentry-multiplexed}/bin/pinentry-multiplexed
+        '';
+        pinentryFlavor = null;
     };
 
-    home.packages = with pkgs; [
-        pinentry_qt
+    home.packages = [
+        pinentry-multiplexed
     ];
 }
