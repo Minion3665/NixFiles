8dee1c0109ad873f3d7859922731c1324a2479ef
Skyler Grey <skyler3665@gmail.com>
2022-08-01 09:36:38 +0100

    Set pull.rebase to merges (removing superseded value)

--- src/nix/system/oss-labs.eu.nix
+++ src/nix/system/oss-labs.eu.nix
@@ -1,17 +1,42 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+  config = { configFile, instanceName }: { pkgs, ... }: {
+    imports = [
+      configFile
+    ];
+
+    _module.args = { instance = instanceName; inputs = {}; };
+  };
+
+in {
   containers.caddy = {
     autoStart = false;
     privateNetwork = true;
-    hostAddress = "192.168.100.10";
-    localAddress = "192.168.100.11";
-    config = import /home/minion/Code/nix/oss-labs.eu/hosts/caddy/default.nix;
+    localAddress = "10.0.108.2";
+    hostAddress = "10.0.108.1";
+    config = config {
+      configFile = /home/minion/Code/nix/oss-labs.eu/hosts/caddy/default.nix;
+      instanceName = "caddy";
+    };
   };
 
   containers.collabora = {
     autoStart = false;
     privateNetwork = true;
-    hostAddress = "192.168.100.10";
-    localAddress = "192.168.100.12";
-    config = import /home/minion/Code/nix/oss-labs.eu/hosts/collabora/default.nix;
+    localAddress = "10.0.108.3";
+    hostAddress = "10.0.108.1";
+    config = config {
+      configFile = /home/minion/Code/nix/oss-labs.eu/hosts/collabora/default.nix;
+      instanceName = "collabora";
+    };
+  };
+
+  networking.nat = {
+    enable = true;
+    internalInterfaces = [ "ve-+" ];
+    externalInterface = "wlan0";
+  };
+
+  networking.hosts = {
+    "10.0.108.2" = [ "collabora.oss-labs.eu" "meet.oss-labs.eu" "notes.oss-labs.eu" ];
   };
 }
