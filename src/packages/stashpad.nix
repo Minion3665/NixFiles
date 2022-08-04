0e00d60dc918a64c05f1ff0c9620a13e835b7349
Skyler Grey <skyler3665@gmail.com>
2022-08-04 10:38:35 +0100

    Fix stashpad derivation

--- src/packages/stashpad.nix
+++ src/packages/stashpad.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: pkgs.appimageTools.wrapType1 {
   name = "StashPad";
-  src = "./stashpad/StashPad.AppImage";
+  src = ./stashpad/StashPad.AppImage;
   extraPkgs = pkgs: with pkgs; [ ];
 }
