be286d2836c79fecfab43070e04474c525cb9693
Skyler Grey <skyler3665@gmail.com>
2022-08-10 20:57:54 +0100

    Stop discord from auto-updating

--- src/apps/personal/discord.nix
+++ src/apps/personal/discord.nix
@@ -1,6 +1,19 @@
 { pkgs, ... }: {
     home.packages = with pkgs; [
         discord
-        discord-ptb
     ];
+
+    home.file.".config/discord/settings.json" = builtins.toJSON {
+        IS_MAXIMIZED = true;
+        IS_MINIMIZED = false;
+        BACKGROUND_COLOR = "#202225";
+        WINDOW_BOUNDS = {
+            x = 485;
+            y = 2341;
+            width = 950;
+            height = 762;
+        };
+        DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
+        SKIP_HOST_UPDATE = true;
+    };
 }
