8e3e75bcf998c370ac754a3d5037a4faed5d9c2d
Skyler Grey <skyler3665@gmail.com>
2022-08-11 11:58:43 +0100

    "done" script is WIP

--- scripts/done.sh
+++ scripts/done.sh
@@ -1,6 +1,6 @@
 # This is a script that integrates with gtimelog
-if [ $# -eq 0 ]; then
-
-else
-    echo "$() $@" >> ~/.local/share/gtimelog/timelog.txt
-fi
+# if [ $# -eq 0 ]; then
+#
+# else
+#    echo "$() $@" >> ~/.local/share/gtimelog/timelog.txt
+# fi
