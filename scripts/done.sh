0a5d5af0a6460b25acb68ed153578fd8042c865d
Skyler Grey <skyler3665@gmail.com>
2022-08-11 12:56:54 +0100

    Add a first working version of the done script

--- scripts/done.sh
+++ scripts/done.sh
@@ -1,6 +1,6 @@
 # This is a script that integrates with gtimelog
-# if [ $# -eq 0 ]; then
-#
-# else
-#    echo "$() $@" >> ~/.local/share/gtimelog/timelog.txt
-# fi
+if [ $# -eq 0 ]; then
+    cat ~/.local/share/gtimelog/timelog.txt | grep "$(date +"^%Y-%m-%d")" | more
+else
+    echo "$(date +"%Y-%m-%d %H:%M"): $@" >> ~/.local/share/gtimelog/timelog.txt
+fi
