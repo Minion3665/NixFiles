8bad2583f00e81afb1110456ce163d7bb77c46f1
Skyler Grey <skyler3665@gmail.com>
2022-08-12 15:17:20 +0100

    Update airline some more

--- src/apps/personal/vim.nix
+++ src/apps/personal/vim.nix
@@ -61,6 +61,26 @@
         let g:airline#extensions#tabline#enabled = 1
         let g:airline#extensions#tabline#formatter = 'unique_tail_improved'
         let g:airline_powerline_fonts = 1
+        let g:airline#extensions#hunks#coc_git = 1
+        let g:airline#extensions#whitespace#enabled = 1
+
+        let g:airline#extensions#tabline#buffer_idx_mode = 1
+        nmap <leader>1 <Plug>AirlineSelectTab1
+        nmap <leader>2 <Plug>AirlineSelectTab2
+        nmap <leader>3 <Plug>AirlineSelectTab3
+        nmap <leader>4 <Plug>AirlineSelectTab4
+        nmap <leader>5 <Plug>AirlineSelectTab5
+        nmap <leader>6 <Plug>AirlineSelectTab6
+        nmap <leader>7 <Plug>AirlineSelectTab7
+        nmap <leader>8 <Plug>AirlineSelectTab8
+        nmap <leader>9 <Plug>AirlineSelectTab9
+        nmap <leader>0 <Plug>AirlineSelectTab0
+        nmap <leader>- <Plug>AirlineSelectPrevTab
+        nmap <leader>+ <Plug>AirlineSelectNextTab
+        let airline#extensions#tabline#middle_click_preserves_windows = 1
+        let g:airline#extensions#scrollbar#minwidth = 100
+        let g:airline_skip_empty_sections = 1
+        let g:airline#extensions#scrollbar#enabled = 1
 
         let g:ctrlp_map = '<c-p>'
         let g:ctrlp_cmd = 'CtrlP'
@@ -170,6 +190,7 @@
 
 
         :let g:airline_highlighting_cache = 1
+        autocmd VimEnter * call vista#RunForNearestMethodOrFunction()
         '';
 
         plugins = [
@@ -199,9 +220,11 @@
             pkgs.vimPlugins.fugitive
             pkgs.vimPlugins.vim-flog
             pkgs.vimPlugins.airline
-            pkgs.vimPlugins.tagbar
+            pkgs.vimPlugins.vista-vim
             pkgs.vimPlugins.vim-gitgutter
             pkgs.vimPlugins.ctrlp-vim
+            pkgs.vimPlugins.vim-airline-clock
+            pkgs.vimPlugins.vim-airline-themes
             (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: pkgs.tree-sitter.allGrammars))
         ];
     };
