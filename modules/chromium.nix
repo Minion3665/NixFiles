b1c89b0c5ccdce36aa7f569caa275a23617486fd
Skyler Grey <skyler3665@gmail.com>
2022-08-26 06:45:48 +0100

    Install a browser
    - I'll be using chromium due to its speed and compatability, which I've found
      firefox has steadily worsened on
    - I'll need to store some of its configuration (i.e. its session) so that we can
      stay logged in over reboots
    - I'll need to store some of its configuration so that keepasss can stay
      connected to it

---
+++ modules/chromium.nix
@@ -0,0 +1,11 @@
+{
+  pkgs,
+  username,
+  ...
+}: {
+  home.home.packages = [pkgs.chromium];
+
+  config.nixpkgs.config.chromium.commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-features=WebUIDarkMode --force-dark-mode --enable-features=WebRTCPipeWireCapturer";
+
+  config.environment.persistence."/nix/persist".users.${username}.directories = [".config/chromium/Default" ".config/chromium/NativeMessagingHosts"];
+}
