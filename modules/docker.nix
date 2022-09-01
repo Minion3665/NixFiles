f6e225176996a13fd14bfbc7d08d7bc084cc851c
Skyler Grey <skyler3665@gmail.com>
2022-09-02 00:03:23 +0100

    Install docker

---
+++ modules/docker.nix
@@ -0,0 +1,14 @@
+{username, ...}: {
+  config = {
+    virtualisation.docker.enable = true;
+
+    users.users.${username}.extraGroups = ["docker"];
+
+    environment.persistence."/nix/persist".directories = [
+      {
+        mode = "0710";
+        directory = "/var/lib/docker";
+      }
+    ];
+  };
+}
