b562da368dbf63d7419caec44348631c3eab0ceb
Skyler Grey <skyler3665@gmail.com>
2022-09-01 23:36:28 +0100

    Install waycorner
    - Waycorner is a hot corners application for wayland
    - It currently appears to be broken some of the time, this needs more
      investigation

---
+++ packages/waycorner.nix
@@ -0,0 +1,40 @@
+{
+  rustPlatform,
+  fetchFromGitHub,
+  cmake,
+  pkg-config,
+  freetype,
+  fontconfig,
+  expat,
+  wayland,
+  system
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "waycorner";
+  version = builtins.substring 0 7 src.rev;
+
+  src = fetchFromGitHub {
+    owner = "Minion3665";
+    repo = "waycorner";
+    rev = "4a42d986cffa66c535082b3f8fb1c7e7bf278272";
+    sha256 = "sha256-OjQPYWtR3a9HZ6h1yXutHlMAfK0G2aRCdtSg9LZh1I0=";
+  };
+
+  cargoLock.lockFile = "${src}/Cargo.lock";
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
+  
+  buildInputs = [
+    freetype
+    fontconfig
+    expat
+    wayland
+  ];
+
+  postFixup = ''
+    patchelf --add-needed ${wayland}/lib/libwayland-client.so $out/bin/waycorner
+  '';
+}
