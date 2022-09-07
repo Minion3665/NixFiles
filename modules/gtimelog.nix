e1d081f52960aaaf5c24d397127c03b78a6106ea
Skyler Grey <skyler3665@gmail.com>
2022-09-07 02:00:25 +0100

    Simplify my collabora gtimelog package expression

--- modules/gtimelog.nix
+++ modules/gtimelog.nix
@@ -10,21 +10,12 @@
       (pkgs.gtimelog.overrideAttrs (oldAttrs: {
         src = gtimelog;
         makeWrapperArgs = [
-          "--set GI_TYPELIB_PATH ${lib.makeSearchPathOutput "lib"
-            "lib/girepository-1.0" (with pkgs; [
-              gtk3
-              libsoup
-              libsecret
-              pango
-              harfbuzz
-              gdk-pixbuf
-              atk
-            ])}"
           "--set GIO_MODULE_DIR ${lib.makeSearchPathOutput "out"
             "lib/gio/modules" (with pkgs; [
               glib-networking
             ])}"
         ];
+        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.gobject-introspection];
         buildInputs = oldAttrs.buildInputs ++ [pkgs.glib-networking];
       }))
     ];
