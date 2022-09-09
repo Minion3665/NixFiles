20b635632dde1a44b4c357c5c071e2617d0c2f6b
Skyler Grey <skyler3665@gmail.com>
2022-09-09 08:15:40 +0100

    Add dependencies for exporting wiki pages

--- modules/neovim/wiki/default.nix
+++ modules/neovim/wiki/default.nix
@@ -2,5 +2,6 @@
   programs.neovim = {
     plugins = [pkgs.wiki-vim];
     extraConfig = builtins.readFile ./wiki.vim;
+    extraPackages = with pkgs; [pandoc texlive.combined.scheme-medium];
   };
 }
