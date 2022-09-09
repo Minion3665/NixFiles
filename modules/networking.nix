e0b3b7d18368c0e3caf97cd031d54967e9447642
Skyler Grey <skyler3665@gmail.com>
2022-09-09 08:14:11 +0100

    Enable eduroam wifi connection

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,6 +1,7 @@
 {config, ...}: {
   config = {
     networking.hostName = "python";
+    networking.useDHCP = true;
 
     networking.wireless.iwd.enable = true;
     networking.wireless.iwd.settings = {
@@ -19,5 +20,16 @@
     hardware.enableRedistributableFirmware = true;
 
     environment.persistence."/nix/persist".directories = ["/var/lib/iwd"];
+
+    sops.secrets."eduroam.8021x" = {
+      sopsFile = ../secrets/eduroam.8021x;
+      format = "binary";
+      path = "/var/lib/iwd/eduroam.8021x";
+    };
+    sops.secrets."eduroam.pem" = {
+      sopsFile = ../secrets/eduroam.pem;
+      format = "binary";
+      path = "/var/lib/iwd/eduroam.pem";
+    };
   };
 }
