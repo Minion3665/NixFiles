1bf9af4f691cb2ebeba27dc1bc48d19591ac985b
Skyler Grey <skyler3665@gmail.com>
2022-09-11 00:53:47 +0100

    Install rust directly rather than per-project

--- modules/rust.nix
+++ modules/rust.nix
@@ -1,5 +1,10 @@
-{username, ...}: {
-  # Rust should be installed via direnv *not* here, however we still need to persist the cache
-  # as we don't want installing crates to be slower than it has to be
+{username, pkgs, ...}: {
+  home.home.packages = with pkgs; [
+    cargo
+    clippy
+    rustc
+    rustfmt
+    bacon
+  ];
   config.environment.persistence."/nix/persist".users.${username}.directories = [".cargo"];
 }
