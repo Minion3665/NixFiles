69b28b92dc7ccff3fda8bd33eee16d585927ccef
Skyler Grey <skyler3665@gmail.com>
2022-09-11 00:42:55 +0100

    Store all of chromium's data
    - Previously we were only storing a single profile and the messaging hosts
    - Some settings are kept in the main directory (i.e. hardware acceleration)

--- modules/chromium.nix
+++ modules/chromium.nix
@@ -7,5 +7,5 @@
 
   config.nixpkgs.config.chromium.commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-features=WebUIDarkMode --force-dark-mode --enable-features=WebRTCPipeWireCapturer";
 
-  config.environment.persistence."/nix/persist".users.${username}.directories = [".config/chromium/Default" ".config/chromium/NativeMessagingHosts"];
+  config.environment.persistence."/nix/persist".users.${username}.directories = [".config/chromium"];
 }
