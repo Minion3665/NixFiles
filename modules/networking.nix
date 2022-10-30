f18fe9fa35d3fddf5bdb917ca76e455e0633b1d2
Skyler Grey <skyler3665@gmail.com>
2022-10-30 23:07:57 +0000

    Re-enable wpa_supplicant declarative config

--- modules/networking.nix
+++ modules/networking.nix
@@ -5,72 +5,53 @@
   ...
 }: {
   config = {
-    environment.systemPackages = [pkgs.nm-tray];
-    networking.hostName = "python";
-    networking.nameservers = ["1.1.1.1" "1.0.0.1"];
+    networking = {
+      hostName = "python";
+      nameservers = ["1.1.1.1" "1.0.0.1"];
+      /* networkmanager = { */
+      /*   enable = true; */
+      /*   wifi.powersave = true; */
+      /*   insertNameservers = ["1.1.1.1" "1.0.0.1"]; */
+      /*   firewallBackend = "nftables"; */
+      /*   unmanaged = ["*"]; */
+      /* }; */
+      search = [
+        "python.local"
+      ];
+      wireless = {
+        enable = true;
+        userControlled.enable = true;
+        networks = {
+          eduroam = {
+            auth = ''
+              scan_ssid=1
+              proto=WPA2
+              key_mgmt=WPA-EAP
+              eap=PEAP
+              pairwise=CCMP
 
-    services.resolved.enable = true;
+              identity="@eduroamUser@"
+              password="@eduroamPass@"
 
-    networking.useNetworkd = true;
-    networking.dhcpcd.enable = false;
-    systemd.network.enable = true;
-    /*
-    networking.networkmanager = {
-    */
-    /*
-    enable = false;
-    */
-    /*
-    wifi = {
-    */
-    /*
-    backend = "iwd";
-    */
-    /*
-    powersave = true;
-    */
-    /*
-    };
-    */
-    /*
-    appendNameservers = ["1.1.1.1" "1.0.0.1"];
-    */
-    /*
-    };
-    */
-
-    /*
-    networking.wireless.dbusControlled = true;
-    */
-
-    networking.wireless.iwd.enable = true;
-    networking.wireless.iwd.settings = {
-      General = {
-        EnableNetworkConfiguration = true;
-      };
-      Settings = {
-        AutoConnect = true;
+              phase2="auth=MSCHAPV2"
+            '';
+          };
+          "HRSFC Guest".psk = "@hrsfcGuestPass@";
+          adelie10 = {
+            psk = "@adelie10Pass@";
+            priority = 500;
+          };
+          robocon-buster_beta.psk = "@roboconProtoboardPass@";
+        };
+        environmentFile = config.sops.secrets."wireless.env".path;
       };
     };
-    networking.search = [
-      "python.local"
-    ];
 
     hardware.enableRedistributableFirmware = true;
 
-    environment.persistence."/nix/persist".directories = ["/var/lib/iwd"];
-
-    sops.secrets."eduroam.8021x" = {
-      sopsFile = ../secrets/eduroam.8021x;
+    sops.secrets."wireless.env" = {
+      sopsFile = ../secrets/wireless.env.bin;
       format = "binary";
-      path = "/var/lib/iwd/eduroam.8021x";
     };
-    sops.secrets."eduroam.pem" = {
-      sopsFile = ../secrets/eduroam.pem;
-      format = "binary";
-      path = "/var/lib/iwd/eduroam.pem";
-    };
-
-    systemd.services.systemd-networkd-wait-online.wantedBy = lib.mkForce [];
   };
 }
