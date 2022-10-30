9f3c0bc4889f035c862aaa30342b0caf62d4775a
Skyler Grey <skyler3665@gmail.com>
2022-10-30 23:24:36 +0000

    Customize the console theme

---
+++ modules/console.nix
@@ -0,0 +1,29 @@
+{
+  config = {
+    boot = {
+      kernelParams = ["consoleblank=60"];
+      consoleLogLevel = 2;
+    };
+    console = {
+      font = "LigaRobotoMono-Regular";
+      colors = [
+        "383a42"
+        "e45649"
+        "50a14f"
+        "c18401"
+        "0184bc"
+        "a626a4"
+        "0997b3"
+        "fafafa"
+        "383a42"
+        "e45649"
+        "50a14f"
+        "c18401"
+        "0184bc"
+        "a626a4"
+        "0997b3"
+        "fafafa"
+      ];
+    };
+  };
+}
