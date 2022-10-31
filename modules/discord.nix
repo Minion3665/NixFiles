6b4317e7f3de2a80f83840cb8ba45a55e4ac178a
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:04:59 +0000

    Install discord with special config options

---
+++ modules/discord.nix
@@ -0,0 +1,23 @@
+{pkgs, ...}: {
+  home.home = {
+    packages = with pkgs; [
+      discord
+    ];
+
+    file.".config/discord/settings.json".text = builtins.toJSON {
+      IS_MAXIMIZED = true;
+      IS_MINIMIZED = false;
+      BACKGROUND_COLOR = "#202225";
+      WINDOW_BOUNDS = {
+        x = 485;
+        y = 2341;
+        width = 950;
+        height = 762;
+      };
+      DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
+      SKIP_HOST_UPDATE = true;
+    };
+  };
+
+  config.internal.allowUnfree = ["discord"];
+}
