96d8f705e3c90cdfa69793a8cb23152727aa506a
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:07:27 +0000

    Install R packages

--- modules/calculator.nix
+++ modules/calculator.nix
@@ -1,3 +1,9 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  R = pkgs.rWrapper.override {
+    packages = with pkgs.rPackages; [
+      zoo
+    ];
+  };
+in {
   home.home.packages = with pkgs; [wcalc R];
 }
