55d50b17a06342c4c067d90e068ba0ad501d687a
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:08:27 +0000

    Change the console font

--- modules/locale.nix
+++ modules/locale.nix
@@ -2,9 +2,6 @@
   config = {
     time.timeZone = "Europe/London";
     i18n.defaultLocale = "en_US.UTF-8";
-    console = {
-      font = "Lat2-Terminus16";
-      keyMap = "uk";
-    };
+    console.keyMap = "uk";
   };
 }
