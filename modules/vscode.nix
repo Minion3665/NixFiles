e027892123b53cbf87e031d5ec07e6ef1b55df17
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:32:10 +0000

    Install vscode-neovim

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -1,6 +1,7 @@
 { pkgs
 , vscode-extensions
 , system
+, home
 , ...
 }: {
   home.programs.vscode = {
@@ -9,6 +10,7 @@
     extensions = with vscode-extensions.packages.${system}; [
       vscode.quandinh.onehalf-dark
       pkgs.vscode-extensions.ms-vsliveshare.vsliveshare
+      pkgs.vscode-extensions.asvetliakov.vscode-neovim
     ];
     mutableExtensionsDir = false;
     userSettings = {
@@ -17,6 +19,10 @@
       "workbench.startupEditor" = "none";
       "files.autoSave" = "afterDelay";
       "files.autoSaveDelay" = 100;
+      "extensions.experimental.affinity" = {
+        "asvetliakov.vscode-neovim" = 1;
+      };
+      "vscode-neovim.neovimExecutablePaths.linux" = "${home.programs.neovim.finalPackage}/bin/nvim";
     };
   };
   config.internal.allowUnfree = [ "vscode-extension-ms-vsliveshare-vsliveshare" ];
