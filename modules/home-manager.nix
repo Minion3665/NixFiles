ef82269991004b60705aefbb82b532c73a98be6b
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:28:24 +0000

    Pass extra stuff to overlays in home-manager too

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,10 +1,15 @@
 { home-manager
 , lib
+, inputs
+, username
+, prismlauncher
 , ...
 }: {
   imports = [ home-manager.nixosModules.home-manager ];
   config = {
     home-manager.useGlobalPkgs = true;
-    nixpkgs.overlays = import ../overlays lib;
+    nixpkgs.overlays = import ../overlays lib (inputs // {
+      inherit username inputs;
+    }) ++ [ prismlauncher.overlay ];
   };
 }
