c25108f394202f958a389b879f371924eb989e95
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:33:28 +0000

    Add some more SSH hosts

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -29,6 +29,19 @@
           user = "minion";
           identityFile = "~/.ssh/id_tildetown";
         };
+        clicks = {
+          hostname = "clicksminuteper.asuscomm.com";
+          port = 5122;
+          user = "minion";
+          identityFile = "~/.ssh/clicks_rsa";
+        };
+        "sshtron.zachlatta.com" = {
+          hostname = "sshtron.zachlatta.com";
+          extraOptions = {
+            PubkeyAcceptedAlgorithms = "+ssh-rsa";
+            HostkeyAlgorithms = "+ssh-rsa";
+          };
+        };
       };
     };
 
