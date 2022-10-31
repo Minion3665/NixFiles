1279259e25d0550ec8d5645d979bb2acad4767f3
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:01:54 +0000

    Add nix-version-diff

---
+++ modules/nix-version-diff.nix
@@ -0,0 +1,12 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  config.system.activationScripts.report-changes = ''
+    PATH=$PATH:${lib.makeBinPath (with pkgs; [nvd nix])}
+    nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2)
+  '';
+
+  home.home.packages = [pkgs.nvd];
+}
