e6acc1609a7d69efc4c1b557d084344086aed3bb
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:19:37 +0000

    Package some more packages

---
+++ packages/analitza.nix
@@ -0,0 +1,27 @@
+{ stdenv,
+  #qt5-declarative,
+  libsForQt5,
+  extra-cmake-modules,
+  #kdoctools,
+  eigen,
+  #qt5-tools,
+  fetchurl,
+}:
+stdenv.mkDerivation {
+  pname = "analitza";
+  version = "21.12.2";
+  dontWrapQtApps = true;
+
+  src = fetchurl {
+    url = "https://download.kde.org/stable/release-service/21.12.2/src/analitza-21.12.2.tar.xz";
+    sha256 = "a4c52d0ea51870495c2da25a58c7495af14e9d71a380d20aea9c1dd39de762aa";
+  };
+
+  buildInputs = [ extra-cmake-modules ];
+  nativeBuildInputs = [
+    libsForQt5.qt5.qtdeclarative
+    libsForQt5.kdoctools
+    libsForQt5.qt5.qttools
+    eigen
+  ];
+}
---
+++ packages/kalgebra.nix
@@ -0,0 +1,30 @@
+{
+  pkgs
+}: with pkgs; let
+  hash = "e82b59795985540062f4c00e582dc42e8b8358e8";
+in
+  stdenv.mkDerivation rec {
+    version = builtins.substring 0 7 hash;
+    src = builtins.fetchGit {
+      url = "https://github.com/KDE/kalgebra";
+      rev = hash;
+    };
+    name = "kalgebra-${version}";
+    nativeBuildInputs = [extra-cmake-modules qt5.wrapQtAppsHook];
+    buildInputs = with qt5; with libsForQt5; [
+      qtbase
+      qtquickcontrols
+      kconfig
+      kcoreaddons
+      kcrash
+      kconfigwidgets
+      kdbusaddons
+      kdoctools
+      ktextwidgets
+      kxmlgui
+      kdeApplications.libkdegames
+      kcompletion
+      analitza
+      kirigami2
+    ];
+  }
