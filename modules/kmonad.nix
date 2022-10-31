967d67eac56802d1cfe85d1181be1030fa2ab7cf
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:04:18 +0000

    Add kmonad to update my keyboard

---
+++ modules/kmonad.nix
@@ -0,0 +1,11 @@
+{ kmonad, ... }: {
+  imports = [ kmonad.nixosModules.default ];
+
+  config.services.kmonad = {
+    enable = false;
+    keyboards.laptop-internal = {
+      device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+      config = builtins.readFile ./kmonad/config.kbd;
+    };
+  };
+}
