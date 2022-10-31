639845fb7837afcfdd679b61967ecf9df351e448
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:13:23 +0000

    Disable KDE connect

--- modules/kdeconnect.nix
+++ modules/kdeconnect.nix
@@ -1,4 +1,8 @@
-{
+{pkgs, ...}: {} # Broken by rename on latest unstable
+/*{
   home.services.kdeconnect.indicator = true;
-  config.programs.kdeconnect.enable = true;  # Can't use home-manager's version because of the firewall
-}
+  config.programs.kdeconnect = {
+    enable = true; # Can't use home-manager's version because of the firewall
+    package = pkgs.plasma5Packages.kdeconnect-kde;
+  };
+}*/
