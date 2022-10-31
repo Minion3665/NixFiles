d4397f1703de19971fb7ed57823cf7fd01eb6803
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:27:35 +0000

    Run a DNS server

---
+++ modules/dnsmasq.nix
@@ -0,0 +1,17 @@
+{ config, ... }: {
+  config = {
+    services.dnsmasq = {
+      enable = true;
+      servers = [ "1.1.1.1" "1.0.0.1" ];
+
+      extraConfig = ''
+        conf-file=${config.sops.secrets."dnsmasq-private.conf".path}
+      '';
+    };
+
+    sops.secrets."dnsmasq-private.conf" = {
+      format = "binary";
+      sopsFile = ../secrets/dnsmasq-private.conf;
+    };
+  };
+}
