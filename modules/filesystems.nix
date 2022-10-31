086093306b8e37bb3315da6f2d4230a0517fadb1
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:31:04 +0000

    Decrease the size of rootfs

--- modules/filesystems.nix
+++ modules/filesystems.nix
@@ -15,7 +15,7 @@
     fileSystems."/" = {
       device = "none";
       fsType = "tmpfs";
-      options = [ "defaults" "size=20G" "mode=755" ];
+      options = [ "defaults" "size=10G" "mode=755" ];
     };
 
     fileSystems."/boot" = {
