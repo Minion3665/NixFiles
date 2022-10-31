7c61e7ae77e6bec689cbe4fbbd2abdef6e1133c6
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:22:11 +0000

    Add a workaround for a steam graphics issue

--- modules/steam.nix
+++ modules/steam.nix
@@ -3,8 +3,6 @@
   username,
   ...
 }: {
-  home.home.packages = [pkgs.steam-run];
-
   config = {
     programs.steam = {
       enable = true;
@@ -13,6 +11,25 @@
     hardware.steam-hardware.enable = true;
 
     internal.allowUnfree = ["steam" "steam-original" "steam-runtime"];
-    environment.persistence."/large/persist".users.${username}.directories = [".local/share/Steam"];
+    environment = {
+      persistence."/large/persist".users.${username}.directories = [".local/share/Steam"];
+      systemPackages = with pkgs; [gamescope gamemode /*(
+        writeTextDir "share/applications/steam.desktop" ''
+          [Desktop Entry]
+          Name=Steam
+          Comment=Application for managing and playing games on steam
+          Exec=${pkgs.gamescope}/bin/gamescope -w 1920 -h 1080 -- ${pkgs.steam}/bin/steam
+          Icon=steam
+          Terminal=false
+          Type=Application
+          Categories=Network;FileTransfer;Game;
+          MimeType=x-scheme-handler/steam;x-scheme-handler/steamlink;
+          Actions=Store;Community;Library;Servers;Screenshots;News;Settings;BigPicture;Friends;
+          PrefersNonDefaultGPU=true
+          X-KDE-RunOnDiscreteGpu=true
+        ''
+      )*/];
+    };
+    boot.kernel.sysctl."dev.i915.perf_stream_paranoid" = "0";
   };
 }
