44570494db33126fe870be3ca42415ee5b177460
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:10:36 +0000

    Use rofi-wayland instead of wofi

--- modules/launcher.nix
+++ modules/launcher.nix
@@ -6,12 +6,12 @@
 }: {
   home = {
     home = {
-      packages = [pkgs.wofi];
-      file.".config/wofi/config".source = ./launcher/config.toml;
+      packages = [pkgs.rofi-wayland];
+      file.".config/rofi/config.rasi".source = ./launcher/config.rasi;
     };
     wayland.windowManager.sway.config.keybindings = lib.mkOptionDefault {
-      "${home.wayland.windowManager.sway.config.modifier}+d" = "exec sh -c '${pkgs.procps}/bin/pkill wofi; ${pkgs.wofi}/bin/wofi'";
-      "${home.wayland.windowManager.sway.config.modifier}+minus" = "exec sh -c '${pkgs.procps}/bin/pkill wofi; ${./launcher/show-scratchpad.sh}'";
+      "${home.wayland.windowManager.sway.config.modifier}+d" = "exec sh -c '${pkgs.procps}/bin/pkill rofi; ${pkgs.rofi-wayland}/bin/rofi -show combi'";
+      "${home.wayland.windowManager.sway.config.modifier}+minus" = "exec sh -c '${pkgs.procps}/bin/pkill rofi; ${./launcher/show-scratchpad.sh}'";
     };
   };
 }
