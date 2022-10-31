3824e3b154b357ce8add2d5580fdf5d08eb38245
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:26:20 +0000

    Allow packages to be built with QT5 stuff

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -1,6 +1,11 @@
 final: prev: let
   lib = prev.lib;
   utils = import ../utils lib;
+
+  extraAttrSets = with final; [
+    libsForQt5
+    libsForQt5.qt5
+  ];
 in
   lib.pipe ../packages [
     utils.nixFilesInWithName
@@ -9,7 +14,11 @@ in
       path,
     }: {
       name = builtins.substring 0 ((builtins.stringLength name) - 4) name;
-      value = final.callPackage path {};
+      value = final.callPackage path (
+        builtins.intersectAttrs
+        (builtins.functionArgs (import path))
+        (lib.fold lib.mergeAttrs {} extraAttrSets)
+      );
     }))
     builtins.listToAttrs
   ]
