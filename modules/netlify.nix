9e675dc51744a598ae8050f40ab0e961bbde0605
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:14:21 +0000

    Store netlify data and let it be a git helper

--- modules/netlify.nix
+++ modules/netlify.nix
@@ -3,7 +3,12 @@
   username,
   ...
 }: {
-  home.home.packages = [pkgs.netlify-cli];
+  home = {
+  programs.git.extraConfig.credential."https://*.netlify.app".helper = "netlify";
+  home = {
+    packages = [pkgs.netlify-cli];
+    sessionPath = ["$HOME/.config/netlify/helper/bin"];
+  };};
 
   config.environment.persistence."/nix/persist".users.${username}.directories = [".config/netlify"];
 }
