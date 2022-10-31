f93bfb385d82d5253622f07227eaaaff1295fba0
Skyler Grey <skyler3665@gmail.com>
2022-10-31 00:03:20 +0000

    Fix a ctrlspace issue by patching it out

--- packages/vim-ctrlspace.nix
+++ packages/vim-ctrlspace.nix
@@ -1,9 +1,9 @@
 { vimUtils, fetchFromGitHub, lib }: vimUtils.buildVimPlugin {
   name = "vim-ctrlspace";
   src = fetchFromGitHub {
-    owner = "vim-ctrlspace";
+    owner = "minion3665";
     repo = "vim-ctrlspace";
-    rev = "05b58e916cea62577462d36bbb88933e8454f2d3";
-    sha256 = "sha256-wQuQTNmU1qujQJ3oU2pSTQaVDa4ZChr1fkjFbILLt+w=";
+    rev = "c1dfc35a208a86256763cbadb6770636b4720b6f";
+    sha256 = "sha256-R3TC56UFEjXNpmmPn5XAw0H07leFt5NR2eKow93NkJI=";
   };
 }
