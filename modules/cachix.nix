63e65bebfb25906a8c084befa8dbfbf5afd38f11
Skyler Grey <skyler3665@gmail.com>
2022-11-02 08:05:01 +0000

    Add cachix and some caches that I own

---
+++ modules/cachix.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }: {
+  config.nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://collabora.cachix.org"
+      "https://nixfiles.cachix.org"
+      "https://cache.nixos.org"
+    ];
+    trusted-public-keys = [
+      "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
+      "collabora.cachix.org-1:e5DYzGux57BzidOmwCJ7NJgydrumaTWtVXdswnerMoU="
+      "nixfiles.cachix.org-1:QGVJNd+0aetjzU5l/MXrVg2adcrm9+8eL0HhIft3F+c="
+    ];
+  };
+  home.home.packages = [ pkgs.cachix ];
+}
