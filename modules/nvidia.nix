c0cfe98c96a75f747106ecc37a0d7584f0f9d9e0
Skyler Grey <skyler3665@gmail.com>
2022-11-03 06:57:25 +0000

    Apply the wiki's fix for screen tearing
    - See <https://nixos.wiki/wiki/Nvidia>'s section on how to "Fix screen tearing"

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -10,7 +10,14 @@ let
 in
 {
   config = {
-    services.xserver.videoDrivers = [ "nvidia" ];
+    services.xserver = {
+      videoDrivers = [ "nvidia" ];
+      screenSection = ''
+        Option         "metamodes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
+        Option         "AllowIndirectGLXProtocol" "off"
+        Option         "TripleBuffer" "on"
+      '';
+    };
     hardware = {
       opengl.enable = true;
       nvidia = {
