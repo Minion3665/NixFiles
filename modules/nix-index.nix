c108348694b9c2c1074bac315c61fc8fbde67f30
Skyler Grey <skyler3665@gmail.com>
2023-01-25 21:32:14 +0000

    Use a cached version of nix-index

--- modules/nix-index.nix
+++ modules/nix-index.nix
@@ -1,10 +1,16 @@
 { pkgs
 , username
+, nix-index-database
 , ...
 }: {
-  home.programs.nix-index = {
-    enable = true;
-    enableZshIntegration = true;
+  home.imports = [ nix-index-database.hmModules.nix-index ];
+
+  home.programs = {
+    nix-index = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    command-not-found.enable = false;
   };
 
   config.environment.persistence."/nix/persist".users.${username}.directories = [ ".cache/nix-index" ];
