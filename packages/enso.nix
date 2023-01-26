87ff387978fa9fec22a2c1e7a6a5433b58802e42
Skyler Grey <skyler3665@gmail.com>
2023-01-26 01:13:22 +0000

    Package enso

--- packages/enso.nix
+++ packages/enso.nix
@@ -1,27 +1,28 @@
-{ lib, rustPlatform, fetchFromGitHub, packageSets, pkg-config, openssl, ... }:
-rustPlatform.buildRustPackage.override
-{
-  rustc = packageSets.fenix.minimal.rustc;
-}
+{ lib
+, fetchFromGitHub
+, pkg-config
+, openssl
+, system
+, _tooling
+, packageSets
+, ...
+}:
+let
+  craneLib = _tooling.crane.lib.${system}.overrideToolchain
+    packageSets.fenix.latest.toolchain;
+in
+craneLib.buildPackage
 rec {
   pname = "enso";
-  version = "2022.1.1-nightly.2022-10-19";
+  version = "2023.1.1-nightly.2023.1.24";
 
-  src = fetchFromGitHub {
+  src = craneLib.cleanCargoSource (fetchFromGitHub {
     owner = "enso-org";
     repo = "enso";
     rev = version;
-    sha256 = "sha256-YCTDxKVzrEDW+LWLyNC/GXh4WfcAuPtwo8VpSllYycI=";
-  };
+    sha256 = "sha256-kwUIIVhw9fb5FlAxlQxcgalIHZ9nz4ey3CSzbQgMyQM=";
+  });
 
   buildInputs = [ openssl.dev ];
   nativeBuildInputs = [ pkg-config ];
-
-  cargoLock = {
-    lockFile = "${src}/Cargo.lock";
-    outputHashes = {
-      "console_error_panic_hook-0.1.7" = "sha256-1RhnLNB04DlsOuBis0bjRLPlJ+hvMF+n+olOaTF2TjM=";
-      "octocrab-0.17.0" = "sha256-0qJvDrwi2FARHyi8597fYJ8V2LuKiZXh9+Hy/2DsBTM=";
-    };
-  };
 }
