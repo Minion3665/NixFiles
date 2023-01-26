13e45bda77c8a0f2fa7106821228276c5d086e7e
Skyler Grey <skyler3665@gmail.com>
2023-01-26 00:39:17 +0000

    Enable a postgresql development server

---
+++ modules/postgres.nix
@@ -0,0 +1,25 @@
+{ lib, config, username, pkgs, ... }: {
+  config = {
+    services = {
+      postgresql = {
+        enable = true;
+        dataDir = "/tmp/postgresql";
+        initialScript = pkgs.writeText "init-postgresql" ''
+          CREATE ROLE ${username} WITH LOGIN PASSWORD '${username}' CREATEDB;
+          GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${username};
+          GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO ${username};
+        '';
+      };
+      pgadmin = {
+        enable = true;
+        initialEmail = "skyler3665@gmail.com";
+        initialPasswordFile = config.sops.secrets.pgadminPassword.path;
+      };
+    };
+    sops.secrets.pgadminPassword = {
+      mode = "0400";
+      owner = config.users.users.pgadmin.name;
+      group = config.users.users.nobody.group;
+    };
+  };
+}
