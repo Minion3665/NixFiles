0c208dbd3ea33a050747a1f17a5cdee7b99a1fac
Skyler Grey <skyler3665@gmail.com>
2023-01-26 00:48:27 +0000

    Install davmail for exchange <-> imap conversion

---
+++ modules/davmail.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }: {
+  home.home = {
+    packages = [pkgs.davmail];
+    file.".davmail.properties".text = ''
+      davmail.server=true
+      davmail.mode=EWS
+      davmail.url=https://outlook.office365.com/EWS/Exchange.asmx
+
+      davmail.imapPort=1143
+      davmail.smtpPort=1025
+    '';
+  };
+}
