4995b47bf9635efa1784b9068254b28876ecb907
Skyler Grey <skyler3665@gmail.com>
2023-01-26 00:51:55 +0000

    Reformat file and plan to install qt6

--- modules/qt.nix
+++ modules/qt.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }: {
-  home.qt = {
-    enable = true;
-    platformTheme = "gnome";
+  home = {
+    qt = {
+      enable = true;
+      platformTheme = "gnome";
 
-    style = {
-      package = pkgs.adwaita-qt;
-      name = "adwaita-dark";
+      style = {
+        package = pkgs.adwaita-qt;
+        name = "adwaita-dark";
+      };
     };
+    /* home.packages = [pkgs.adwaita-qt6]; */
   };
 }
