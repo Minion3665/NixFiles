0cfe689b060351f718f7f53e9c590500ea316c1b
Skyler Grey <skyler3665@gmail.com>
2023-01-26 00:46:11 +0000

    Add the nix-community cachix key

--- modules/cachix.nix
+++ modules/cachix.nix
@@ -10,6 +10,7 @@
       "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
       "collabora.cachix.org-1:e5DYzGux57BzidOmwCJ7NJgydrumaTWtVXdswnerMoU="
       "nixfiles.cachix.org-1:QGVJNd+0aetjzU5l/MXrVg2adcrm9+8eL0HhIft3F+c="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
   home.home.packages = [ pkgs.cachix ];
