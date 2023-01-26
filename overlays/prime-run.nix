46d80663f918432dc060e49826ea81015cae0710
Skyler Grey <skyler3665@gmail.com>
2023-01-26 00:46:50 +0000

    Create an overlay to wrap applications with prime-run

---
+++ overlays/prime-run.nix
@@ -0,0 +1,16 @@
+final: prev: let
+  programs = [
+    "steam"
+    "prismlauncher"
+  ];
+in prev.lib.pipe programs [
+  (map (name: {
+    inherit name;
+    value = prev.${name}.overrideAttrs (prevAttrs: {
+      postInstall = (prevAttrs.postInstall or "") + ''
+        sed -i 's/^Exec=/&prime-run /g' $out/share/applications/*.desktop
+      '';
+    });
+  }))
+  builtins.listToAttrs
+]
