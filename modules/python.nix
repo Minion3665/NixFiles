152228b850beb224e50043be12e126b1c8494489
Skyler Grey <skyler3665@gmail.com>
2023-02-13 08:01:00 +0000

    Add a workaround to make apparmor tools work

--- modules/python.nix
+++ modules/python.nix
@@ -1,8 +1,13 @@
 { pkgs, ... }: {
-  home.home.packages = [
-    (pkgs.python3.withPackages
-      (pyPkgs: with pyPkgs; [
-        rope
-      ]))
-  ];
+  home.home = {
+    packages = [
+      (pkgs.python3.withPackages
+        (pyPkgs: with pyPkgs; [
+          rope
+        ]))
+    ];
+    sessionVariables = {
+      PYTHONPATH = "${pkgs.apparmor-utils}/lib/python3.10/site-packages:${pkgs.libapparmor.python}/lib/python3.10/site-packages";
+    };
+  };
 }
