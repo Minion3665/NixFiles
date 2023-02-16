04b4a198b9a5e7d945af613ce8d5fa98f81fb404
Skyler Grey <skyler3665@gmail.com>
2023-02-16 16:20:31 +0000

    Persist freecad configuration

--- modules/design.nix
+++ modules/design.nix
@@ -1,3 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, username, ... }: {
   home.home.packages = [ pkgs.freecad pkgs.openscad ];
+  config.environment.persistence."/large/persist".users.${username}.directories = [ ".local/share/FreeCAD" ];
 }
