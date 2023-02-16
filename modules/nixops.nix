abc575892dcb77a2a498f02876c5d247b881fcd3
Skyler Grey <skyler3665@gmail.com>
2023-02-16 16:21:00 +0000

    Replace the insecure nixops with the later nixopsUnstable

--- modules/nixops.nix
+++ modules/nixops.nix
@@ -1,8 +1,3 @@
 { pkgs, ... }: {
-  home.home.packages = [ pkgs.nixops ];
-  config.nixpkgs.config.permittedInsecurePackages = [
-    "python-2.7.18.6"
-    "python2.7-certifi-2021.10.8"
-    "python2.7-pyjwt-1.7.1"
-  ];
+  home.home.packages = [ pkgs.nixopsUnstable ];
 }
