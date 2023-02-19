38d5eb47dcccba8b12f88ab2e2e2bb96d8fb2ed4
Skyler Grey <skyler3665@gmail.com>
2023-02-19 23:48:52 +0000

    Rename the qemu module to be more general aliases

--- modules/aliases.nix
+++ modules/aliases.nix
@@ -3,5 +3,6 @@
     vm = "sudo nixos-rebuild build-vm --flake .#email";
     build = "sudo nixos-rebuild switch --flake .#default";
     build-nosub = "sudo nixos-rebuild switch --flake .#default --option substitute false";
+    mnt = "sudo mkdir /mnt -p && sudo mount --target /mnt";
   };
 }
