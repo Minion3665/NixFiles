5cf56dd636b099e146e5266bbc891cda5f797097
Skyler Grey <skyler3665@gmail.com>
2023-02-26 12:42:07 +0000

    Blacklist loading unnecessary kernel modules on boot

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,11 +1,33 @@
-{
+{ pkgs, options, lib, ... }: {
   config = {
-    boot.loader = {
-      systemd-boot = {
-        enable = true;
-        editor = false;
+    boot = {
+      loader = {
+        systemd-boot = {
+          enable = true;
+          editor = false;
+        };
+        efi.canTouchEfiVariables = true;
       };
-      efi.canTouchEfiVariables = true;
+      kernelPackages = pkgs.linuxPackages_zen;
+      blacklistedKernelModules = [
+        "bridge"
+        "loop"
+        "macvlan"
+        "msr"
+        "nf_nat_ftp"
+        "razercore"
+        "razerfirefly"
+        "razerkbd"
+        "razerkraken"
+        "razermouse"
+        "razermug"
+        "tap"
+        "tun"
+        "uinput"
+        "veth"
+      ];
     };
   };
+
+  traces = [ "config.boot.kernelModules" "config.boot.loader.systemd-boot.editor" ];
 }
