73d247f3986258f63cf36c9772e119370e9d3d63
Skyler Grey <skyler3665@gmail.com>
2023-02-26 12:43:52 +0000

    Avoid dns caching at the nscd level as we already use dnsmasq

--- modules/dnsmasq.nix
+++ modules/dnsmasq.nix
@@ -1,12 +1,15 @@
 { config, ... }: {
   config = {
-    services.dnsmasq = {
-      enable = true;
-      servers = [ "1.1.1.1" "1.0.0.1" ];
+    services = {
+      nscd.enableNsncd = true;
+      dnsmasq = {
+        enable = true;
+        servers = [ "1.1.1.1" "1.0.0.1" ];
 
-      extraConfig = ''
-        conf-file=${config.sops.secrets."dnsmasq-private.conf".path}
-      '';
+        extraConfig = ''
+          conf-file=${config.sops.secrets."dnsmasq-private.conf".path}
+        '';
+      };
     };
 
     sops.secrets."dnsmasq-private.conf" = {
