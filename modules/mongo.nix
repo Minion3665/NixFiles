c7ab4a4260ada2203281e7207bec9641e2963aaf
Skyler Grey <skyler3665@gmail.com>
2023-02-26 12:43:10 +0000

    Install mongo (but don't load it on boot)

--- modules/mongo.nix
+++ modules/mongo.nix
@@ -1,11 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   config = {
     services.mongodb = {
       package = pkgs.mongodb-6_0;
-      enable = false;
+      enable = true;
       dbpath = "/tmp/mongodb";
     };
     internal.allowUnfree = [ "mongodb" "mongodb-compass" ];
+    systemd.services.mongod.wantedBy = lib.mkForce [];
   };
 
   home.home.packages = [ pkgs.mongodb-compass ];
