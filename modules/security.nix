d2642a2619267d10c4698de661c0f03056217df1
Skyler Grey <skyler3665@gmail.com>
2023-02-26 12:47:10 +0000

    Enable the fingerprint driver

--- modules/security.nix
+++ modules/security.nix
@@ -8,6 +8,15 @@ let
 in
 {
   config = {
+    internal.allowUnfree = [ "libfprint-2-tod1-goodix" ];
+    services.fprintd = {
+      enable = true;
+      tod = {
+        enable = true;
+        driver = pkgs.libfprint-2-tod1-goodix;
+      };
+    };
+
     security.apparmor = {
       enable = true;
       killUnconfinedConfinables = true;
