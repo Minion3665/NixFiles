f5f798956598a6758ae136ea2bf90db2916c0713
Skyler Grey <skyler3665@gmail.com>
2023-03-07 13:13:45 +0000

    Change the display configuration to match new inputs

--- modules/displays.nix
+++ modules/displays.nix
@@ -1,11 +1,26 @@
-{
+{ lib, ... }: {
+  config = {
+    boot.kernelParams = [
+      "video=eDP-1:2256x1440@60"
+      "video=HDMI-A-1:3840x2160@76"
+    ];
+    services.xserver.config = lib.mkAfter ''
+      Section "Device"
+        Identifier "Device-radeon[1]"
+        Driver     "ati" 
+        BusID      "PCI:06:00.0"
+        Option     "AllowExternalGpus" "True"
+        Option     "AllowEmptyInitialConfiguration"
+      EndSection
+    '';
+  };
   home.services.grobi = {
     enable = true;
     rules = [
       {
         name = "Docked";
-        outputs_connected = [ "HDMI-1-0" "eDP-1" ];
-        configure_column = [ "HDMI-1-0" "eDP-1" ];
+        outputs_connected = [ "HDMI-A-1" "eDP-1" ];
+        configure_column = [ "HDMI-A-1" "eDP-1" ];
 
         atomic = true;
         primary = "eDP-1";
@@ -13,7 +28,7 @@
       {
         name = "Free";
         outputs_connected = [ "eDP-1" ];
-        outputs_disconnected = [ "HDMI-1-0" ];
+        outputs_disconnected = [ "HDMI-A-1" ];
         configure_single = "eDP-1";
       }
       {
