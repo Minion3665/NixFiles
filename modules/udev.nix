1fcb0b2a7a8d45e22e5e2e5ad82fb83580bc53b3
Skyler Grey <skyler3665@gmail.com>
2023-03-19 19:46:17 +0000

    Add platformio udev rules

---
+++ modules/udev.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }: {
+  config.services.udev.packages = [ (pkgs.stdenv.mkDerivation {
+    pname = "udev-rules";
+    version = "2023-03-18";
+
+    src = ./udev/rules;
+
+    installPhase = ''
+      mkdir -p $out/etc/udev/rules.d/
+      cp $src/* $out/etc/udev/rules.d/
+    '';
+  }) ];
+}
