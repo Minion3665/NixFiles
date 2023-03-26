684ed13cc4a5c0e8e40eba12dc8eb516d0f31894
Skyler Grey <skyler3665@gmail.com>
2023-03-26 10:45:48 +0000

    Make my headset reconnect on login

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -6,4 +6,10 @@
     };
     environment.persistence."/nix/persist".directories = [ "/var/lib/bluetooth" ];
   };
+  home = {
+    home.file.".config/prelaunch/bluetooth-reconnect.sh".text = ''
+      bluetoothctl connect E8:EE:CC:04:84:5F
+    '';
+    home.file.".config/prelaunch/bluetooth-reconnect.sh".executable = true;
+  };
 }
