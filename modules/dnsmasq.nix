6808c213c9747fde48a7de7963e3d076e30f4846
Skyler Grey <skyler3665@gmail.com>
2023-04-06 09:45:29 +0000

    Enable expanding .local addresses with avahi

--- modules/dnsmasq.nix
+++ modules/dnsmasq.nix
@@ -8,6 +8,22 @@
           server = [ "1.1.1.1" "1.0.0.1" ];
           conf-file = config.sops.secrets."dnsmasq-private.conf".path;
         };
+        extraConfig = ''
+          local=/local/
+          domain=local
+          expand-hosts
+        '';
+      };
+      avahi = {
+        enable = true;
+        nssmdns = true;
+        ipv4 = true;
+        ipv6 = false;
+        publish = {
+          enable = true;
+          addresses = true;
+          workstation = true;
+        };
       };
     };
 
