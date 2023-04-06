112dbebf9443c86f37926b6127528244f7b78193
Skyler Grey <skyler3665@gmail.com>
2023-04-06 18:04:53 +0000

    Update the build alias to cap core count

--- modules/aliases.nix
+++ modules/aliases.nix
@@ -1,7 +1,7 @@
 {
   home.home.shellAliases = {
     vm = "sudo nixos-rebuild build-vm --flake .#email";
-    build = "nice -n 19 sudo nixos-rebuild switch --flake .#default";
+    build = "nice -n 19 sudo nixos-rebuild switch --flake .#default --cores 10";
     build-nosub = "nice -n 19 sudo nixos-rebuild switch --flake .#default --option substitute false";
     mnt = "sudo mkdir /mnt -p && sudo mount --target /mnt";
   };
