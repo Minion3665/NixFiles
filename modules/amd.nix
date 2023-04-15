79a8dc10560a647447d14b26372adcedf58c0d1c
Skyler Grey <skyler3665@gmail.com>
2023-04-15 14:35:41 +0000

    Replace amdgpu with the modesetting driver to avoid X crashes

--- modules/amd.nix
+++ modules/amd.nix
@@ -8,23 +8,24 @@
     home.file.".config/prelaunch/amd-output-source.sh".executable = true;
   };
   config = {
-    boot.initrd.kernelModules = [ "radeon" "amdgpu" ];
-    services.xserver.videoDrivers = [ "amdgpu" "radeon" "modesetting" "fbdev" ];
+    boot.initrd.kernelModules = [ "amdgpu" ];
+    services.xserver.videoDrivers = [ "modesetting" "fbdev" ];
+    /* services.xserver.defaultDepth = 24; */
     systemd.tmpfiles.rules = [
       "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.hip}"
     ];
     hardware.opengl.driSupport = true;
     hardware.opengl.driSupport32Bit = true;
     services.hardware.bolt.enable = true;
-    services.xserver.config = lib.mkAfter ''
-      Section "Device"
-        Identifier "Device-amdgpu[1]"
-        Driver     "amdgpu" 
-        BusID      "PCI:06:00.0"
-        Option     "AllowExternalGpus" "True"
-        Option     "AllowEmptyInitialConfiguration"
-        Option     "TearFree" "true"
-      EndSection
-    '';
+    /* services.xserver.config = lib.mkAfter '' */
+    /*   Section "Device" */
+    /*     Identifier "Device-radeon[1]" */
+    /*     Driver     "radeon" */ 
+    /*     BusID      "PCI:06:00.0" */
+    /*     Option     "AllowExternalGpus" "True" */
+    /*     Option     "AllowEmptyInitialConfiguration" */
+    /*     # Option     "TearFree" "true" */
+    /*   EndSection */
+    /* ''; */
   };
 }
