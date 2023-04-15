ae2576cdc3d25bccfaa33b7c2b6bd9a999b8aa6a
Skyler Grey <skyler3665@gmail.com>
2023-04-15 14:36:12 +0000

    Enable planetscale utilities

---
+++ modules/hosting.nix
@@ -0,0 +1,8 @@
+{ pkgs, username, ... }: {
+  home.home.packages = [
+    pkgs.mysql pkgs.pscale  # Planetscale cli
+  ];
+  config.environment.persistence."/large/persist".users.${username}.directories = [
+    ".config/planetscale"
+  ];
+}
