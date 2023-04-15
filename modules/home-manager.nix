4436541495373b5fcb83f485820eb370a7ecabd6
Skyler Grey <skyler3665@gmail.com>
2023-04-15 14:36:33 +0000

    Make home-manager symlink .nix-profile to avoid activation failures

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -11,4 +11,11 @@
       inherit username inputs;
     });
   };
+  home.home.activation.linkProfile = home-manager.lib.hm.dag.entryBefore [
+    "installPackages"
+  ] ''
+    if [ ! -d /home/${username}/.nix-profile/ ]; then
+      ln -s /nix/var/nix/profiles/per-user/${username}/profile/ /home/${username}/.nix-profile
+    fi
+  '';
 }
