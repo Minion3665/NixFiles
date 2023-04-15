ccb566b76bc30682ccc916b19623581214842470
Skyler Grey <skyler3665@gmail.com>
2023-04-15 20:18:40 +0000

    Remove hybridbar

--- modules/gsettings.nix
+++ modules/gsettings.nix
@@ -3,7 +3,7 @@
     packages = with pkgs; [ gsettings-desktop-schemas glib ];
     sessionVariables = {
       XDG_DATA_DIRS =
-        "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:${pkgs.hybridbar}/share/gsettings-schemas/${pkgs.hybridbar.name}:$XDG_DATA_DIRS";
+        "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS";
     };
   };
 }
