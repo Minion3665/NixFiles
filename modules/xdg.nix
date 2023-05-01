d0a7ce649a92274395291ffcff20644b14ff56a3
Skyler Grey <skyler3665@gmail.com>
2023-05-01 08:08:46 +0000

    Install xdg-user-dirs for scripts

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  home.home.packages = [ pkgs.xdg-utils ];
+  home.home.packages = [ pkgs.xdg-utils pkgs.xdg-user-dirs ];
   config.xdg.portal.enable = false;
   # ^ portal is enabled in .xinitrc as this didn't work
 }
